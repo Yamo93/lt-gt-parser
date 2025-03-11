ba6a6d985d35ddf1d6fedc3864d68d0c9308d87a
Yamo Gebrewold <yamo.gebrewold@gmail.com>
2025-03-11 10:30:03 +0100

    Updates

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+document.addEventListener('DOMContentLoaded', function () {
+    var inputField = document.getElementById('inputField');
+    var submitButton = document.getElementById('submitButton');
+    var outputField = document.getElementById('outputField');
+    submitButton.addEventListener('click', function () {
+        var userInput = inputField.value;
+        var output = parseLtGt(userInput);
+        outputField.textContent = "The parsed string is: ".concat(output);
+    });
+});
+function parseLtGt(userInput) {
+    return userInput
+        .replace("&lt;", "<")
+        .replace("&gt;", ">");
+}
